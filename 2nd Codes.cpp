69a962f51162f63f8e92ea68747eaa3c635036fc
[kazi-Saeed-Hossain] <kazisaeed8056@gmail.com>
2023-01-20 14:12:57 +0600

    Add a new file 2nd Codes.cpp

---
+++ 2nd Codes.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    char n;
+
+    cout<<"Enter a letter = ";
+    cin>>n;
+
+    switch(n)
+    {
+        case 'a':
+        cout<<"You entered a vowel letter !!!";
+        break;
+
+        case 'e':
+        cout<<"You entered a vowel letter !!!";
+        break;
+
+        case 'i':
+        cout<<"You entered a vowel letter !!!";
+        break;
+
+        case 'o':
+        cout<<"You entered a vowel letter !!!";
+        break;
+
+        case 'u':
+        cout<<"You entered a vowel letter !!!";
+        break;
+
+        default:
+            cout<<"You entered a consonant letter !!!";
+
+    }
+    return 0;
+}
